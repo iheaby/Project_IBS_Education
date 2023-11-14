38e7efdae6f08b2635ca81d92930a59aabb31d4a
iheaby <iheaby@gmail.com>
2023-11-14 19:17:54 +0300

    Итоговая задача 2 решена

--- src/Itogovie_zadaniya/Itog2.java
+++ src/Itogovie_zadaniya/Itog2.java
@@ -6,9 +6,10 @@ import java.util.Scanner;
 public class Itog2 {
     public static void main(String[] args) {
         String uravnenieStr, strtrim;
-        boolean vernoe;
+        boolean vernoe,tojevernoe;
         int indexplus, indexminus,indexX,indexeq;
         int result = 0;
+        int trueresult, resultabs;
         System.out.println("Введите уравнение: ");
         Scanner uravnenie = new Scanner((System.in));
         uravnenieStr = uravnenie.nextLine();
@@ -18,49 +19,55 @@ public class Itog2 {
         indexminus = strtrim.indexOf('-');
         indexX = strtrim.indexOf('x');
         indexeq = strtrim.indexOf('=');
-
+        int ch0 = Character.getNumericValue(strtrim.charAt(0));
+        int ch2 = Character.getNumericValue(strtrim.charAt(2));
+        int ch4 = Character.getNumericValue(strtrim.charAt(4));
         if((indexeq==3)&(indexplus==1)&(strtrim.length()==5)){
             vernoe=true;
         }
         else if ((indexeq==3)&(indexminus==1)&(strtrim.length()==5)){
-            vernoe=true;
+            tojevernoe=true;
         }
         else{
             System.out.println("Уравнение не является простым!");
+            return;
         }
-        if (vernoe=true&indexX==0&indexplus==1){
-            result = result + (uravnenieStr.indexOf(4) - uravnenieStr.indexOf(2));
-            System.out.println("Вывод: " + result);
+        if(indexplus==1) {
+            if (indexX == 0) {
+                result = ch4 - ch2;
+                System.out.println("Вывод: " + result);
 
+            } else if (indexX == 2) {
+                result = ch4 - ch0;
+                System.out.println("Вывод: " + result);
+
+            } else if (indexX == 4) {
+                result = ch0 + ch2;
+                System.out.println("Вывод: " + result);
+
+            }
         }
-        else if (vernoe==true&indexX==2&indexplus==1) {
-            result = Integer.valueOf(strtrim.indexOf(4)) - Integer.valueOf(strtrim.indexOf(0));
-            System.out.println("Вывод: " + result);
+        if (indexminus==1) {
+            if (indexX == 0) {
+                result = ch4 + ch2;
+                System.out.println("Вывод: " + result);
 
-        }
-
-        else if (vernoe==true&indexX==4&indexplus==1) {
-            result = Integer.valueOf(strtrim.indexOf(0)) + Integer.valueOf(strtrim.indexOf(2));
-            System.out.println("Вывод: " + result);
-
-        }
-
-        else if (vernoe==true&indexX==0&indexminus==1) {
-            result = Integer.valueOf(strtrim.indexOf(4)) + Integer.valueOf(strtrim.indexOf(2));
-            System.out.println("Вывод: " + result);
-
-        }
-
-        else if (vernoe==true&indexX==2&indexminus==1) {
-            result =  (-1)*(Integer.valueOf(strtrim.indexOf(4)) - Integer.valueOf(strtrim.indexOf(2)));
-            System.out.println("Вывод: " + result);
-
-        }
-
-        else if (vernoe==true&indexX==4&indexminus==1) {
-            result = Integer.valueOf(strtrim.indexOf(0)) - Integer.valueOf(strtrim.indexOf(2));
-            System.out.println("Вывод: " + result);
-
+            } else if (indexX == 2 & indexminus == 1) {
+                result = ch4 - ch0;
+                resultabs = Math.abs(result);
+                if (result < 0) {
+                    trueresult = resultabs;
+                    System.out.println("Вывод: " + trueresult);
+                }
+                else {
+                    trueresult = -1 * resultabs;
+                    System.out.println("Вывод: " + trueresult);
+                }
+            }
+            else if (indexX == 4 & indexminus == 1) {
+                result = ch0 - ch2;
+                System.out.println("Вывод: " + result);
+            }
         }
     }
 }
